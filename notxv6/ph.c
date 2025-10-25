49cecb739af7f8112eb0f164ca5b4882224d79d2
231220000-Zhang San <huixinyuan06@126.com>
2025-10-25 10:19:31 -0400

    complete

--- notxv6/ph.c
+++ notxv6/ph.c
@@ -16,8 +16,7 @@ struct entry {
 struct entry *table[NBUCKET];
 int keys[NKEYS];
 int nthread = 1;
-
-
+pthread_mutex_t lock[NBUCKET];
 double
 now()
 {
@@ -40,7 +39,7 @@ static
 void put(int key, int value)
 {
   int i = key % NBUCKET;
-
+  pthread_mutex_lock(&lock[i]);
   // is the key already present?
   struct entry *e = 0;
   for (e = table[i]; e != 0; e = e->next) {
@@ -54,7 +53,7 @@ void put(int key, int value)
     // the new is new.
     insert(key, value, &table[i], table[i]);
   }
-
+    pthread_mutex_unlock(&lock[i]);
 }
 
 static struct entry*
@@ -110,6 +109,9 @@ main(int argc, char *argv[])
     fprintf(stderr, "Usage: %s nthreads\n", argv[0]);
     exit(-1);
   }
+  for(int i=0;i<NBUCKET;i++){
+    pthread_mutex_init(&lock[i],NULL);
+  }
   nthread = atoi(argv[1]);
   tha = malloc(sizeof(pthread_t) * nthread);
   srandom(0);
@@ -117,7 +119,7 @@ main(int argc, char *argv[])
   for (int i = 0; i < NKEYS; i++) {
     keys[i] = random();
   }
-
+   
   //
   // first the puts
   //
--- user/uthread.c
+++ user/uthread.c
@@ -10,10 +10,28 @@
 #define STACK_SIZE  8192
 #define MAX_THREAD  4
 
+struct context {
+  uint64 ra;  // 返回地址 (Return Address)
+  uint64 sp;  // 栈指针 (Stack Pointer)
 
+  // 被调用者保存的寄存器 (Callee-saved registers)
+  uint64 s0;
+  uint64 s1;
+  uint64 s2;
+  uint64 s3;
+  uint64 s4;
+  uint64 s5;
+  uint64 s6;
+  uint64 s7;
+  uint64 s8;
+  uint64 s9;
+  uint64 s10;
+  uint64 s11;
+};
 struct thread {
   char       stack[STACK_SIZE]; /* the thread's stack */
   int        state;             /* FREE, RUNNING, RUNNABLE */
+  struct context context;
 };
 struct thread all_thread[MAX_THREAD];
 struct thread *current_thread;
@@ -62,6 +80,7 @@ thread_schedule(void)
      * Invoke thread_switch to switch from t to next_thread:
      * thread_switch(??, ??);
      */
+    thread_switch((uint64)&t->context,(uint64)&current_thread->context);
   } else
     next_thread = 0;
 }
@@ -76,6 +95,10 @@ thread_create(void (*func)())
   }
   t->state = RUNNABLE;
   // YOUR CODE HERE
+  t->context.ra=(uint64)func;
+  t->context.sp=(uint64)&t->stack+STACK_SIZE;
+
+  
 }
 
 void 
@@ -160,3 +183,5 @@ main(int argc, char *argv[])
   thread_schedule();
   exit(0);
 }
+//a 0x10
+// 0x3bc
